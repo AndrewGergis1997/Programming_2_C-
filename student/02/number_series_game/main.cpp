068b553d8cf93a8eb4a7e6712386e3029ba640cb
Andrew Gergis <mqange@linux-desktop9.tuni.fi>
2022-09-17 18:10:50 +0300

    Reading the input and printing the numbers

---
+++ student/02/number_series_game/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "How many numbers would you like to have? ";
+
+    // Write your code here
+
+    int num = 0 ;
+    int i;
+    cin >>i;
+    for ( ; i>=num;num++)
+      {
+     cout<<num+1<< endl;
+      }
+      return 0;
+}
