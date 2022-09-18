a8771fe3df5f4356066b73aef5c515241cc694c8
Andrew Gergis <mqange@linux-desktop1.tuni.fi>
2022-09-18 16:54:50 +0300

    cube

---
+++ student/02/cube/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  cout << "Enter a number: ";
+  int n;
+  int cube = 0;
+  cin >> n;
+  if (n <= 1289)
+    {
+        cube = pow (n,3);
+        cout <<"The cube of "<< n <<" is " << cube << endl;
+    }
+  else
+    {
+       cube = pow (n,3);
+      cout << "Error! The cube of "<< n << "is not "<< cube <<"." << endl;
+    }
+
+  return 0;
+}
