fd1630ce65887d6035458a7e6ad52e4dc04952f6
Andrew Gergis <mqange@linux-desktop7.tuni.fi>
2022-11-02 02:59:41 +0200

    Completed all checks

--- student/07/gigs/main.cpp
+++ student/07/gigs/main.cpp
@@ -35,13 +35,18 @@
 
 using namespace std;
 
-
+struct Gig{
+  string artistName,
+  date,
+  city,
+  stage;
+};
 
 // Farthest year for which gigs can be allocated
 const string FARTHEST_POSSIBLE_YEAR = "2030";
 
 // Casual split func, if delim char is between "'s, ignores it.
- vector<string> split(const string& str, char delim = ';')
+vector<string> split(const string& str, char delim = ';')
 {
   vector<string> result = {""};
   bool inside_quotation = false;
@@ -76,7 +81,7 @@ bool isFormat(ifstream & file_obj)
         {
           if(line[i] == ';' && line[i+1]==';')
             return false;
-          if(line[i] == ';')
+          else if(line[i] == ';')
             count++;
         }
       if(count!= 3)
@@ -126,222 +131,104 @@ bool is_valid_date(const std::string& date_str)
 }
 
 
-bool checkDate (ifstream &file2)
+bool checkDate (vector <struct Gig> &artistData)
 {
   set <string> dateData;
   string line;
   vector <string> dataItems;
-  while(getline(file2, line))
+  for( uint i=0; i < artistData.size();i++)
     {
-      dataItems = split (line);
-      if (!is_valid_date(line))
+
+      if (!is_valid_date(artistData.at(i).date))
         {
           return false;
         }
-      dateData.insert(dataItems.at(1));
     }
-  file2.close();
   return true;
 }
 
-bool checkStages (ifstream &file4)
+
+bool checkArtist (vector <struct Gig> &artistData)
 {
-  set <string> StageData;
-  string line;
-  vector <string> dataItems;
-  while(getline(file4, line))
-    {
-      dataItems = split (line);
-      StageData.insert(dataItems.at(4));
-    }
-  for(uint i=0; i<dataItems.size();i++)
-    {
-      for (uint j=0; j<dataItems.size();j++)
-        {
-          if (dataItems.at(i)==dataItems.at(j))
-            {
-              return false;
-            }
-        }
-    }
-  file4.close();
+  map<string,vector<string>> artistSched;
+  for(uint i=0; i < artistData.size() ; i++){
+vector<string> schedList;
+if(artistSched.count(artistData.at(i).artistName)){
+//retreive the schedule list of the artist
+//schedList = artistSched.find(artistData.at(i).artistName);
+//Check if the date list doesn't contain the new date
+//if(check_sched(schedList,artistData.at(i).date))
+
+if (artistSched.find(artistData.at(i).artistName)==artistSched.end())
+return false;
+}
+schedList.push_back(artistData.at(i).date);
+artistSched.insert({artistData.at(i).artistName,schedList});
+  }
   return true;
 }
 
-//struct Gig{
-//  string artist,
-//  date,
-//  city,
-//  stage;
-//};
 
-set <string> readArtistDataFromFile (ifstream &file1)
+void readDataFromFile (ifstream &file1,vector <struct Gig> &artistData )
 {
-  set <string> artistData;
+  struct Gig gig1;
   string line;
   vector <string> dataItems;
   while(getline(file1, line))
     {
       dataItems = split (line);
-      artistData.insert(dataItems.at(0));
+      gig1.artistName=dataItems.at(0);
+      gig1.date=dataItems.at(1);
+      gig1.city=dataItems.at(2);
+      gig1.stage=dataItems.at(3);
+
+      artistData.push_back(gig1);
     }
   file1.close();
-  return artistData;
 }
 
 
-set <string> readDateDataFromFile (ifstream &file2)
+
+void printArtist (vector <struct Gig>  &artistData)
 {
-  set <string> dateData;
-  string line;
-  vector <string> dataItems;
-  while(getline(file2, line))
+  set <string> gig;
+  for (uint i=0; i<artistData.size();i++)
     {
-      dataItems = split (line);
-      dateData.insert(dataItems.at(1));
+      gig.insert(artistData.at(i).artistName);
     }
-  file2.close();
-  return dateData;
-}
-
-set <string> readCityDataFromFile (ifstream &file3)
-{
-  set <string> cityData;
-  string line;
-  vector <string> dataItems;
-  while(getline(file3, line))
-    {
-      dataItems = split (line);
-      cityData.insert(dataItems.at(2));
-    }
-  file3.close();
-  return cityData;
-}
-
-set <string> readStageDataFromFile (ifstream &file4)
-{
-  set <string> stageData;
-  string line;
-  vector <string> dataItems;
-  while(getline(file4, line))
-    {
-      dataItems = split (line);
-      stageData.insert(dataItems.at(3));
-    }
-  file4.close();
-  return stageData;
-}
-
-void printArtist (set <string>  &gig)
-{
   for (auto & itr : gig)
     {
       cout << itr << endl;
     }
 }
 
-void printDate (set <string>  &gig)
-{
-  for (auto & itr : gig)
-    {
-      cout << itr << endl;
-    }
-}
-//set<int> convertToSet(vector<int> v)
+
+
+//void printDate (vector <struct Gig>  &artistData)
 //{
-//  // Declaring the set
-//  set<int> s;
-
-//  // Traverse the Vector
-//  for (int x : v) {
-
-//      // Insert each element
-//      // into the Set
-//      s.insert(x);
-//    }
-
-//  // Return the resultant Set
-//  return s;
-//}
-
-//struct Gig{
-//  string artist,
-//  date,
-//  city,
-//  stage;
-//};
-
-
-//void checkCommand(std::string & command, std::vector<Gig> gig_data,
-//                  std::set<std::string> artistsData)
-//{
-
-//    std::vector<std::string> commandParts;
-//    char delim = ' ';
-//    commandParts = split(command, delim);
-//    if(commandParts.size() < 1)
+//  set <string> gig;
+//  for (uint i=0; i<artistData.size();i++)
 //    {
-//        std::cout << "Error: Invalid input." << std::endl;
+//      gig.insert(artistData.at(i).date);
 //    }
-//    if(commandParts.size() == 1)
+//  for (auto & itr : gig)
 //    {
-//        if(commandParts.at(0) != "ARTISTS" && commandParts.at(0) != "STAGES" )
-//        {
-//            std::cout << "Error: Invalid input." << std::endl;
-//        }
+//      cout << itr << endl;
 //    }
-//    if (commandParts.size()== 1 && commandParts.at(0) == "ARTISTS")
-//    {
-//        print_artist(artistsData);
-//    }
-//    if (commandParts.size()== 1 && commandParts.at(0) == "STAGES")
-//    {
-//        std::cout << "All gig places in alphabetical order:" << std::endl;
-//        std::cout << gig2.city << " , " << gig2.stage << std::endl;
-//    }
-//    if(commandParts.size() == 2)
-//    {
-//        std::string secpos = commandParts.at(1);
-//        for(auto & a: gig_data)
-//            secpos = a;
-//        if(commandParts.at(0) == "ARTIST" && commandParts.at(1) != a)
-//        {
-//            std::cout << "Error: Not found." << std::endl;
-//        }
-//    }
-//    if(commandParts.size() == 2)
-//    {
-//        if(commandParts.at(0) == "STAGE" && commandParts.at(1) !=  gig2.stage)
-//        {
-//            std::cout << "Error: Not found." << std::endl;
-//        }
-//    }
-
-//    commandParts.clear();
 //}
 
 
 int main()
 {
-  set <string> artistData;
-  set <string> artistDate;
-  set <string> artistCity;
-  set <string> artistStage;
+  vector <struct Gig> artistData;
+
   string inFile = "";
   cout << "Give a name for input file: ";         // input file name
   getline(cin, inFile);
 
   // creating files to read from it
   ifstream fileRead(inFile);
-  ifstream file1(inFile);
-  artistData=readArtistDataFromFile (file1);
   ifstream file2(inFile);
-  artistDate=readDateDataFromFile(file2);
-  ifstream file3(inFile);
-  artistCity=readCityDataFromFile(file3);
-  ifstream file4(inFile);
-  artistStage=readStageDataFromFile(file4);
-
   // check for input file errors
   if ( not fileRead ) {
       cout << "Error: File could not be read."<< endl;
@@ -349,47 +236,37 @@ int main()
     }
 
   // check for format file errors
-  else if (not isFormat(fileRead))
+  if (not isFormat(file2))
     {
       cout << "Error: Invalid format in file."<< endl;
       return EXIT_FAILURE;
     }
+  readDataFromFile (fileRead,artistData);
 
   // check for date data errors
-  else if (not checkDate(file2))
+  if (not checkDate(artistData))
     {
       cout << "Error: Invalid date."<< endl;
       return EXIT_FAILURE;
     }
 
   // check for stage data errors
-  else if (not checkStages(file4))
+  if (not checkArtist(artistData))
     {
       cout << "Error: Already exists."<< endl;
       return EXIT_FAILURE;
     }
 
+//  if (not checkstage(artistData))
+//    {
+//      cout << "Error: Already exists."<< endl;
+//      return EXIT_FAILURE;
+//    }
   //   reading data into a file structure
 
 
 
   //  set <string> artists = convertToSet(gigData.at(0));
-
-
-//  std::string command;
-//  while (command != "QUIT")
-//  {
-//      std::cout << "gigs> ";
-//      getline(std::cin, command);
-//      for (auto & c: command)
-//      {
-//          c = toupper(c);
-//      }
-//      checkCommand(command, gig_data, artistsData);
-
-
-//  }
-
 
 
 
@@ -406,14 +283,9 @@ int main()
         {
           cout<< "All artists in alphabetical order:"<<endl;
           printArtist(artistData);
-          printDate(artistDate);
         }
     }
   fileRead.close();
-  file1.close();
-  file2.close();
-  file3.close();
-  file4.close();
 
   return EXIT_SUCCESS;
 }
